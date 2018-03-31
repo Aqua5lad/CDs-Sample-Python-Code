6e50658a8aff0ca4f3e242d7a66c0bd471948b67
Colm Doherty <G00364795@gmit.ie>
2018-03-31 17:26:00 +0100

    Create test1.py
    test to understand the effect of While, If and Else statements

---
+++ test1.py
@@ -0,0 +1,12 @@
+# testing commands & logic by Colm Doherty
+# Using While, If, Else, Print.
+
+n = 20
+
+while n <= 100:
+    if (n % 2 == 0):
+        print("this number", n, "is even") 
+    else:  
+        print("this number", n, "is odd")
+    n = n + 1
+    
