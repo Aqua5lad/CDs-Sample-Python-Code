1d39cd4459bab8a3addf897a6d3b8cc0f8a570c3
Colm Doherty <G00364795@gmit.ie>
2018-03-31 21:22:08 +0100

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,18 @@
+# FizzBuzz.py is a program to learn WHILE, IF, ELSE, ELIF, and % modulo (the remainder after dividing)
+# It was written by Ian McLoughlin on 2018-02-01
+# FizzBuzz: https://en.wikipedia.org/wiki/Fizz_buzz
+# note the logic of if,else, then(represented by :) 
+
+i = 1
+
+while i <= 300: 
+    if (i % 3 == 0) and (i % 5 ==0):
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print ("Fizz")
+    elif i % 5 ==0:
+        print("Buzz")
+    else:          
+        print(i)
+    i = i +1
+
