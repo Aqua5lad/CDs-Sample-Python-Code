e7f996caed58333c1ed4a0a3c77c51d363d5fb5d
Colm Doherty <G00364795@gmit.ie>
2018-04-05 20:35:58 +0100

    Create ColumnValues.py

---
+++ ColumnValues.py
@@ -0,0 +1,19 @@
+# Colm Doherty 2018-4-4
+
+# experimenting with selecting individual columns of data from the Iris data sets 
+# so as to allow calculations of max, min and mean values
+
+with open("data/iris.csv") as f:
+            # open(filename) returns the file to be used, in this case the Iris dataset.
+            # For best practice, its prefaced by 'with' to ensure the file is closed afterwards
+
+    for line in f:        
+            # for each line in the dataset
+
+
+        def total = 1
+                numbers = [1, 2, 3]
+                numsum = (sum(numbers))
+
+# ---------
+
