63276020b706f26d9b523e5a31f3a6e3acd8dc58
Colm Doherty <G00364795@gmit.ie>
2018-04-21 11:07:03 +0100

    test new function

--- FuncTest.py
+++ FuncTest.py
@@ -6,6 +6,8 @@
 import numpy as np
 # import the dataset
 data = np.genfromtxt('Data/Iris.csv', delimiter=',')
+
+# --------------------
 # FOR TESTING - the existing organic code (it works):
 col1mean = (np.mean(data[:,0]))
 print("Column 1 mean is:",'{:0.3f}'.format(col1mean))
@@ -23,12 +25,9 @@ print("Column 4 mean is:",'{:0.3f}'.format(colmean(3)))
 # YAY! this took over 2hrs of trial & error to get right, with help from Ian's "Defining functions" video
 # and a helluva of lotta noodling back & forth
 
-# Another possible method - untested so far:
-# def colmean = (np.mean(data[r,c]))
-    # where r = row number, and c = column number
-    # print("mean of column [c] for range in row[r] is: ")
+# ---------------------
 
-#define the new function for column max:
+#now define the new function for column max:
 def colmax(colno):
     maxcol = (np.max(data[:,colno]))
     return maxcol    
@@ -39,6 +38,7 @@ print("Column 3 max is:",'{:0.1f}'.format(colmax(2)))
 print("Column 4 max is:",'{:0.1f}'.format(colmax(3)))
 
 
+# this was the original code for finding Col Max
 col1max = (np.max(data[:,0]))
     # find the max of the values in Column 1 (guessed this, since .mean gave the mean
 col2max = (np.max(data[:,1]))
@@ -49,3 +49,32 @@ print("Column 1 max is:",'{:0.3f}'.format(col1max))
 print("Column 2 max is:",'{:0.3f}'.format(col2max))
 print("Column 3 max is:",'{:0.3f}'.format(col3max))
 print("Column 4 max is:",'{:0.3f}'.format(col4max))    
+
+# ------------------------------
+
+ # now I want to call a range of rows in each column ...
+ # this is the original code:
+
+col1Setomean = (np.mean(data[0:49,0]))
+print("Column 1 Setosa mean is:",'{:0.3f}'.format(col1Setomean))
+    # figured this out with help from http://cs231n.github.io/python-numpy-tutorial/#numpy-arrays
+col2Setomean = (np.mean(data[0:49,1]))
+col3Setomean = (np.mean(data[0:49,2]))
+col4Setomean = (np.mean(data[0:49,3]))
+
+print("Column 2 Setosa mean is:",'{:0.3f}'.format(col2Setomean))
+print("Column 3 Setosa mean is:",'{:0.3f}'.format(col3Setomean))
+print("Column 4 Setosa mean is:",'{:0.3f}'.format(col4Setomean))
+    # displays the mean for each Column in the Setosa sample
+
+# here is the NEW code:
+
+def colmeanS(colno):
+    meancolS = (np.mean(data[0:49,colno]))
+    return meancolS
+
+print("(C3) Setosa P.L. mean is:",'{:0.3f}'.format(colmeanS(2)))
+
+
+
+ 
